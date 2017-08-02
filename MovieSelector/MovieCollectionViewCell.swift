ee621cf0047c6becb83691098ca79e38ec43ee6e
Alex Beattie <artisanbranding@me.com>
2017-08-02 12:29:16 -0700

    working today

---
+++ MovieSelector/MovieCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCollectionViewCell.swift
+//  MovieSelector
+//
+//  Created by Alex Beattie on 7/31/17.
+//  Copyright © 2017 Artisan Branding. All rights reserved.
+//
+
+import UIKit
+
+public class MovieCollectionViewCell: UICollectionViewCell {
+    @IBOutlet public weak var movieImageView: UIImageView!
+    @IBOutlet public weak var movieTitleLable: UILabel!
+}
